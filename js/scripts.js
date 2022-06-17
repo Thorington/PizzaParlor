d1a844bb8dfc75a06f9e2d308e6a813fabbfcc83
Claire Thorington <claire.thorington@gmail.com>
2022-06-17 16:03:06 -0700

    WIP commit, struggling with functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,56 @@
 
-// Business Logic
+// // Business Logic
 
-function customPizza(size, toppings) {
+function Pizza([toppings], size) {
+  this.toppings = [toppings];
   this.size = size;
-  this.toppings = toppings;
 }
 
-customPizza.prototype.Order = function() {
-  return this.size + " " + this.toppings + " " + this.price;
-}
+const myPizza = new Pizza(["anchovies, pineapple"], "medium");
 
 
+
+// UI Logic
+
+let PizzaOrder = new Pizza([toppings], size);
+
+$(document).ready(function () {
+  attachContactListeners();
+  $("form#options").submit(function (event) {
+    event.preventDefault();
+
+    const inputtedToppings = $("input:checkbox[name=pizza-topping]:checked").val();
+    const inputtedSize = $('input:radio[name=size]:checked').val();
+
+    $("input:checkbox[name=pizza-topping]:checked").val();
+    $('input:radio[name=size]:checked').val();
+
+    let PizzaOrder = new Pizza(inputtedToppings, inputtedSize);
+    $("#cart").html(PizzaOrder + "");
+    
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// // Business Logic
+// // function CustomPizza(toppings, size) {
+// //   this.toppings = toppings;
+// //   this.size = size;
+// // }
+
+// // CustomPizza.prototype.Order = function() {
+// //   return this.size + " " + this.toppings;
+// // };
 
 
 
@@ -40,14 +80,26 @@ customPizza.prototype.Order = function() {
 
 // UI Logic
 
-$(document).ready(function() {
-  $("form#options").submit(function(event) {
-    event.preventDefault();
-    $("#cart").show();
-    $("input:checkbox[name=pizza-topping]:checked").each(function(){
-      const toppings = $(this).val();
-      $("#cart").append(toppings + "<br>");
-    });
-    $('#options').hide();
-  });
-});
+// $(document).ready(function() {
+
+//   $("form#options").submit(function (event) {
+//     event.preventDefault();
+//     $("#cart").show();
+//     $("input:checkbox[name=pizza-topping]:checked").each(function () {
+//       const toppings = $(this).val();
+//       $("#cart").append(toppings + "<br>");
+//     });
+//     $('#options').hide();
+//     });
+//   });
+
+
+
+
+  // $("form#options2").submit(function (event) {
+    //   event.preventDefault();
+    //   $("#options2").show();
+    //   $('input:radio[name=size]:checked').each(function () {
+    //     const size = $(this).val();
+    //     $("#finalCart").append(toppings + "<br>");
+    //   });
