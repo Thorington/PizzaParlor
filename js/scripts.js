4fe80285fb45ecf0695cc6ad8d9966c17bad2afa
Claire Thorington <claire.thorington@gmail.com>
2022-06-17 11:33:55 -0700

    add initial js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,44 @@
 
+// Business Logic
 
+Total.prototype.calculateTotal = function() {
+
+  const basePrice = 10;
+  let output = [basePrice + (toppings * 2)];
+  if (size === Medium) {
+    output[0] -= 2;
+  }
+  if (size === Large) {
+    output[0] += 2;
+  }
+  return output;
+};
+
+function cost(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+  this.price = this.calculateTotal();
+}
 
 
 // UI Logic
 
 $(document).ready(function() {
-  $("form#toppings").submit(function(event) {
+  $("form#options").submit(function(event) {
     event.preventDefault();
     $("#cart").show();
     $("input:checkbox[name=pizza-topping]:checked").each(function() {
-      const chosenToppings = $(this).val();
-      $('#cart').append(chosenToppings + "<br>");
+      const chosenOptions = $(this).val();
+      $('#cart').append(chosenOptions + "<br>");
     });
-    $('#toppings').hide();
+    $('#options').hide();
   });
 });
+
+function getOrder() {
+  const toppings = $('#toppings').text();
+  const size = $('#size').text();
+  const price = ((toppings * $2) + $10);
+  
+  return new Order(toppings, size, price);
+}
